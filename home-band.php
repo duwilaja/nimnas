da3f3230cc5c7f10950a820f1492e56c8cf49495
duwilaja <apaaja.software@gmail.com>
2023-10-31 16:17:25 +0700

    band

--- home-band.php
+++ home-band.php
@@ -1,14 +1,44 @@
-										<tr>
-											<td>1</td>
-											<td class="coin_icon mt-2 d-flex">
-												<span class=" my-auto">116.68.172.242</span>
-											</td>
-											<td>CMG5000-BIN-[MASTER]</td>
-											<td>BIN - PEJATEN</td>
-											<td><a class="btn ripple btn-info" data-bs-target="#modaldemo3" data-bs-toggle="modal" href="">View</a></td>
-											<td>ROUTER</td>
-										</tr>
-										<tr>
+<?php
+$redirect=false;
+$cleartext=true;
+
+include "inc.common.php";
+include "inc.session.php";
+include "lib_inc.db.php";
+
+$conn=connect();
+
+$whr=($mys_LOC=='')?"1=1":" loc in ('$mys_LOC')";
+
+$ord=post("ord");
+
+$sql="select hostname,name,loc,type,sum(t.ifinoctets_delta) as inb,sum(t.ifoutoctets_delta) as outb 
+from core_traffic t join ports p on t.port_id=p.port_id join devices x on x.device_id=t.device_id join nimdb.core_node n on x.hostname=n.host 
+where date(dtm)=date(now()) and $whr 
+group by hostname,name,loc,type 
+ order by inb $ord limit 5";
+
+$rs=exec_qry($conn,$sql);
+$lists=fetch_alla($rs);
+
+disconnect($conn);
+
+for($i=0;$i<count($lists);$i++){
+	$list=$lists[$i];
+?>
+		<tr>
+			<td><?php echo ($i+1) ?></td>
+			<td class="coin_icon mt-2 d-flex">
+				<span class=" my-auto"><?php echo $list['hostname'] ?></span>
+			</td>
+			<td><?php echo $list['name'] ?></td>
+			<td><?php echo $list['loc'] ?></td>
+			<td><a class="btn ripple btn-info" data-bs-target="#modaldemo3" data-bs-toggle="modal" href=""><?php echo $list['inb'] ?></a></td>
+			<td><?php echo $list['type'] ?></td>
+		</tr>
+<?php }?>
+
+										<!--tr>
 											<td>2</td>
 											<td class="coin_icon mt-2 d-flex">
 												<span class=" my-auto">116.68.172.242</span>
@@ -47,4 +77,4 @@
 											<td>BIN - PEJATEN</td>
 											<td><a class="btn ripple btn-info" data-bs-target="#modaldemo3" data-bs-toggle="modal" href="">View</a></td>
 											<td>ROUTER</td>
-										</tr>
+										</tr-->
