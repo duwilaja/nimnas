a86c980503ec1d050ca03e45fda9433f1b35a18d
duwilaja <apaaja.software@gmail.com>
2024-03-18 11:03:12 +0700

    petagugel

---
+++ petagugel.php
@@ -0,0 +1,206 @@
+<?php 
+include "inc.common.php";
+//include "inc.session.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lokasi</title>
+	<meta charset="utf-8" />
+
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Icons css-->
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+	
+<style type="text/css">
+html, body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
+</head>
+<body>
+&nbsp;
+	<div id="map" style="position: absolute; top: 0px; left: 0; width: 100%; height: 100%;"></div>
+&nbsp;
+<!-- Jquery js-->
+<script src="spruha/assets/plugins/jquery/jquery.min.js"></script>
+
+<script type="module">
+import {
+  MarkerClusterer
+} from "https://cdn.skypack.dev/@googlemaps/markerclusterer@2.3.1";
+
+let ext = '<?php echo $ext?>';
+
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+    key: "AIzaSyB0LcVlAmmXMro8eH69aK6Wh4lUqttz-Zs",
+    v: "weekly",
+    // Use the 'v' parameter to indicate the version to use (weekly, beta, alpha, etc.).
+    // Add other bootstrap parameters as needed, using camel case.
+  });
+
+
+async function initMap(locations) {
+  // Request needed libraries.
+  const { Map, InfoWindow } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
+    "marker",
+  );
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 5, //3,
+    center: {lat: -2,lng: 118}, //{ lat: -28.024, lng: 140.887 },
+    mapId: "DEMO_MAP_ID",
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: "",
+    disableAutoPan: true,
+  });
+  
+  
+  // Create an array of alphabetical characters used to label the markers.
+  //const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  // Add some markers to the map.
+  //const markers = locations.map((position, i) => {
+    /*const label = labels[i % labels.length];
+    const pinGlyph = new google.maps.marker.PinElement({
+      glyph: label,
+      glyphColor: "white",
+    });
+	*/
+	var err=''; var markers=[];
+   for (var i = 0; i < locations.length; i++) {
+		var a = locations[i];
+		var title = a['name']+'\nTotal: '+a['cnt']+'\nON: '+a['onoff']+'\nOFF: '+a['off']+'\nLink: '+a['lnk']+'\nBW: '+a['bw'];
+		var color = a['onoff']>0?"1":"0";
+		
+		if(isNaN(a['lat'])||isNaN(a['lng'])){
+			err+=a['name']+'/';
+		}else{
+			const myLatLng = new google.maps.LatLng(a['lat'], a['lng']);
+
+			const iconImage = document.createElement("img");
+			iconImage.src = "img/"+color+".png";
+			const marker = new google.maps.Marker({
+			  position: myLatLng,
+			  content: iconImage,//pinGlyph.element,
+			  title: title,
+			});
+			
+			markers.push(marker);
+
+			// markers can only be keyboard focusable when they have click listeners
+			// open info window when marker is clicked
+			//marker.addListener("click", () => {
+			  //infoWindow.setContent(position.lat + ", " + position.lng);
+			  //infoWindow.open(map, marker);
+			//});
+				//return marker;
+		}
+  };
+
+  // Add a marker clusterer to manage the markers.
+  new MarkerClusterer({ markers, map });
+}
+/*
+const locations = [
+  { lat: -31.56391, lng: 147.154312 },
+  { lat: -33.718234, lng: 150.363181 },
+  { lat: -33.727111, lng: 150.371124 },
+  { lat: -33.848588, lng: 151.209834 },
+  { lat: -33.851702, lng: 151.216968 },
+  { lat: -34.671264, lng: 150.863657 },
+  { lat: -35.304724, lng: 148.662905 },
+  { lat: -36.817685, lng: 175.699196 },
+  { lat: -36.828611, lng: 175.790222 },
+  { lat: -37.75, lng: 145.116667 },
+  { lat: -37.759859, lng: 145.128708 },
+  { lat: -37.765015, lng: 145.133858 },
+  { lat: -37.770104, lng: 145.143299 },
+  { lat: -37.7737, lng: 145.145187 },
+  { lat: -37.774785, lng: 145.137978 },
+  { lat: -37.819616, lng: 144.968119 },
+  { lat: -38.330766, lng: 144.695692 },
+  { lat: -39.927193, lng: 175.053218 },
+  { lat: -41.330162, lng: 174.865694 },
+  { lat: -42.734358, lng: 147.439506 },
+  { lat: -42.734358, lng: 147.501315 },
+  { lat: -42.735258, lng: 147.438 },
+  { lat: -43.999792, lng: 170.463352 },
+];
+*/
+//initMap();
+
+$(document).ready(function(){
+	loadLoc();
+});
+
+function gohome(){
+	//map.setView([-2,118], 5);
+}
+
+function loadLoc(){
+	var err='';
+	$.ajax({
+		type: 'POST',
+		url: 'dataget'+ext,
+		data: {q:'map',id:-1},
+		success: function(datax){
+			var data=JSON.parse(datax)["msgs"];
+			initMap(data);
+		},
+		error: function(xhr){
+			console.log(xhr);
+		}
+	});
+}
+
+function loadMarker(){
+	gohome();
+	if(markers!=null)  {
+		map.removeLayer(markers);
+		markers.clearLayers();
+		//$("#tot").val(0);
+	}
+	markers=L.markerClusterGroup();
+	var err='';
+	$.ajax({
+		type: 'POST',
+		url: 'dataget'+ext,
+		data: {q:'map',id:-1},
+		success: function(datax){
+			var data=JSON.parse(datax)["msgs"];
+			for (var i = 0; i < data.length; i++) {
+				var a = data[i];
+				var title = a['name']+'\nTotal: '+a['cnt']+'\nON: '+a['onoff']+'\nOFF: '+a['off']+'\nLink: '+a['lnk']+'\nBW: '+a['bw'];
+				var color = a['onoff']>0?"1":"0";
+				//var icon = L.AwesomeMarkers.icon({icon: 'server', prefix: 'fa', markerColor: color});
+				var icon = L.icon({iconUrl:'img/'+color+'.png',iconSize:[30,30],iconAnchor:[15,30]});
+				
+				if(isNaN(data[i]['lat'])||isNaN(data[i]['lng'])){
+					err+=data[i]['name']+'/';
+				}else{
+					var marker = L.marker(new L.LatLng(a['lat'], a['lng']), { title: title, icon: icon });
+					
+					//var fn=markerClickFunction(a['locid']);
+					//marker.on('click', fn);
+					
+					//marker.addTo(map);
+					markers.addLayer(marker);
+				}
+			}
+			markers.addTo(map);
+			//$("#tot").val(data.length);
+			if(err!='') {
+				alert('Error: '+err);
+			}
+		},
+		error: function(xhr){
+			console.log(xhr);
+		}
+	});
+}
+</script>
+</body>
+</html>
