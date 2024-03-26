9b0bcff65d6306eb9cd4e006a83932fa2f20779a
duwilaja <apaaja.software@gmail.com>
2024-03-26 14:51:18 +0700

    tickhom

--- tick_hom.php
+++ tick_hom.php
@@ -65,7 +65,7 @@ disconnect($conn);
 		
 			<!-- row opened -->
 			<div class="row row-sm">
-				<div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+				<div class="col-sm-12 col-md-6 col-lg-6 col-xl-4">
 					<div class="card custom-card bg-danger blink divlnk" onclick="tickets('new');">
 						<div class="card-body">
 							<div class="card-order">
@@ -115,7 +115,7 @@ disconnect($conn);
 					</div>
 				</div>
 				<!-- COL END -->
-				<div class="col-sm-12 col-md-6 col-lg-6 col-xl-3">
+				<div class="col-sm-12 col-md-6 col-lg-6 col-xl-2">
 					<div class="card custom-card bg-primary">
 						<div class="card-body">
 							<div class="card-order">
