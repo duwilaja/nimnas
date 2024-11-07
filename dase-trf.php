51388dde406a45b126fe2bff8c32fa7a9aabe7d4
duwilaja <apaaja.software@gmail.com>
2024-11-07 16:24:08 +0700

    dase trf

--- dase-trf.php
+++ dase-trf.php
@@ -8,16 +8,10 @@ include "lib_inc.db.php";
 
 $conn=connect();
 
-$wherloc ='where hostname in (select host from nimdb.core_node)';
+$wherloc ='';
 $host='';
 if($mys_LOC!=''){
-	$sql="select host from nimdb.core_node where loc in ('$mys_LOC')";
-	$rs=fetch_alla(exec_qry($conn,$sql));
-	$aho=array();
-	for($i=0;$i<count($rs);$i++){
-		$aho[]=$rs[$i]['host'];
-	}
-	$wherloc.=count($aho)>0?" and hostname in ('".implode("','",$aho)."')":"";
+	$wherloc=" where hostname in (select host from nimdb.core_node where loc in ('$mys_LOC'))";
 }
 $sql="select hostname from devices $wherloc";
 $rs=fetch_alla(exec_qry($conn,$sql));
